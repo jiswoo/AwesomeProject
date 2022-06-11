0d313e0b9b9f0b1b74a968dc2921f2a76a9c5913
PhanKhacCuong <cuong.phan2@mservice.com.vn>
2022-06-12 01:05:11 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/**
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+    StyleSheet
+} from 'react-native';
+import DetailScreen from './DetailScreen';
+
+
+const App = () => {
+    return (
+        <DetailScreen />
+    );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
